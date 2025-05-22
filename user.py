05d6f05f441cf60d17a1798e78ff5c3d74f5b32c
AdityaJoshi-14 <joshiaditya174@gmail.com>
2025-05-22 10:33:48 +0530

    Create user.py

---
+++ user.py
@@ -0,0 +1,8 @@
+name = input("Enter Your Name:")
+age = int(input("Enter Your Age:"))
+print(f"Hello {name}, you are {age} years old")
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
